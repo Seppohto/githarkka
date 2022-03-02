85c54ef236c89da37f35c4f2684e2e168def0598
seppohto <olli.uronen@gmail.com>
2022-03-02 16:49:51 +0200

    Made a new class Person

--- git_pro.py
+++ git_pro.py
@@ -1,2 +1,28 @@
 def hello():
     return "Hello World!"
+
+class Person :
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+ 
+    def __repr__(self):
+        return '{' + self.name + ', ' + str(self.age)+ '}'
+
+    # getter method
+    def get_name(self):
+        return self._name
+      
+    # setter method
+    def set_name(self, x):
+        self._name = x
+    
+    # getter method
+    def get_age(self):
+        return self._age
+      
+    # setter method
+    def set_age(self, x):
+        self._age = x
+
